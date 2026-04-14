4d034dd155ac7bc662e917c797a8684597dfbbde
iSyIt <isy.htb.ctf@gmail.com>
2026-04-14 16:10:31 +0200

    Added alot idk

--- src/compiler.c
+++ src/compiler.c
@@ -5,22 +5,35 @@
 #define MAX_TOKENS 100
 
 typedef enum {
-    TOKEN_PLUS,
-    TOKEN_MINUS,
-    TOKEN_ASSIGN,
-    TOKEN_NUMBER
+  TOKEN_PLUS,
+  TOKEN_MINUS,
+  TOKEN_ASSIGN,
+  TOKEN_NUMBER
 } TokenType;
 
 typedef struct {
-    TokenType type;
-    int value;
+  TokenType type;
+  int value; // Only used for numbers
 } Token;
+
+// This is genuinely hard to explain, but basically, we are basically making another kind of Token struct, but this one points to what is below it, to know the order.
+
+typedef struct {
+  TokenType type;
+  int value; // Only used for numbers
+  struct AbstractTreeNode* left;
+  struct AbstractTreeNode* right;
+} AbstractTreeNode; 
+
+
 
 void Lexer(const char *input, int *count, Token *TokensReturned) { 
     // For loop for the input  *input is just the "char string"  *count is just where the count is updated  *TokensReturned, obviously is where the Tokens are returned
     for (int i = 0; input[i] != '\0'; i++) {
         char c = input[i];
-
+        if (isspace(c)) {
+          continue;
+        }
         if (isdigit(c)) { // Case for being a number
             int num = 0;  // start accumulator at 0
             while (isdigit(input[i])) {
@@ -75,18 +88,22 @@ int main(int argc, char* argv[]) {
     // for (int i = 0; i<count; i++) {
     //     printf("Value: %d\nType: %d\n\n", backTokens[i].value, backTokens[i].type);
     // }
-    if (argc < 2) {
-        fprintf(stderr, "Bro ur legit dumb asf. It's %s <input>\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Bro ur legit dumb asf. It's %s <input>\n", argv[0]); // Probably will change this later lol
         return 1;
     }
     char *cli_input = argv[1];
     char *cli_output = argv[2];
 
 
-    char output_file;
+    int output_file;
     FILE *file_pointer = fopen(cli_input, "r");
+
+    if (!file_pointer) {
+      perror("fopen failed reason");
+    }
     while ((output_file = fgetc(file_pointer)) != EOF) {
-        printf("%c", output_file);
+        fprintf(f, "%c", output_file);
     }
 
 
